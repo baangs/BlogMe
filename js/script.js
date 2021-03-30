6289b6caa3e4745e418abb68b3c996fdc8a2b108
baangs <aey.baangs@gmail.com>
2021-03-29 17:27:07 -0700

    major changes

---
+++ js/script.js
@@ -0,0 +1,9 @@
+const headerItems = document.querySelectorAll('.header-item')
+const iconHam = document.getElementById('.header-img')
+const headerMenu = document.getElementById('.header-menu')
+
+headerItems.forEach(item => {
+    item.addEventListener('click', (e) => {
+        
+    })
+})
